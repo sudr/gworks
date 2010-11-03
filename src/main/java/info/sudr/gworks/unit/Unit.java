adec494d944e7e1aea26f08c33e3ee56743ecbd7
sudr <to_sud@yahoo.com>
2010-11-02 23:18:13 -0500

    fleshed out unit

--- src/main/java/info/sudr/gworks/unit/Unit.java
+++ src/main/java/info/sudr/gworks/unit/Unit.java
@@ -1,13 +1,18 @@
 package info.sudr.gworks.unit;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 public class Unit {
 
-	private HashMap<String, Object> properties = new HashMap<String,Object>();
+	private HashMap<String, Object> properties;
 	private final String type;
+	private int id;
+	private String name;
+	private Set<Weapon> weapons;
 	
 	public Unit(String type) {
 		this.type = type;
@@ -17,12 +22,47 @@ public class Unit {
 		return type;
 	}
 	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	public void setProperty(String propertyName, Object propertyValue) {
+		if (properties == null) {
+			properties = new HashMap<String,Object>();
+		}
 		properties.put(propertyName, propertyValue);
 	}
 	
 	public Object getProperty(String propertyName) {
-		return properties.get(propertyName);
+		if (properties == null) {
+			throw new RuntimeException("No properties have been set");
+		}
+		Object value = properties.get(propertyName);
+		
+		if (value == null) {
+			throw new RuntimeException("Property " + propertyName + " has not been set");
+		}
+		
+		return value;
+	}
+	
+	public void addWeapon(Weapon weapon) {
+		if (weapons == null) {
+			weapons = new HashSet<Weapon>();
+		}
+		weapons.add(weapon);
 	}
 	
 	@Override
--- src/test/story/info/sudr/gworks/unit/InteractWithUnitStory.groovy
+++ src/test/story/info/sudr/gworks/unit/InteractWithUnitStory.groovy
@@ -6,6 +6,14 @@ shared_behavior "shared behaviors", {
 	}
 }
 
+scenario "the type on an airplane unit", {
+	it_behaves_as "shared behaviors"
+	
+	then "the unit's type  must be an airplane", {
+		unit.getType().shouldBeEqualTo "airplane"
+	}
+}
+
 scenario "setting the speed on an airplane unit", {
 	it_behaves_as "shared behaviors"
 	
@@ -18,10 +26,59 @@ scenario "setting the speed on an airplane unit", {
 	}
 }
 
-scenario "the type on an airplane unit", {
+scenario "modifying the speed on an airplane unit", {
 	it_behaves_as "shared behaviors"
 	
-	then "the unit's type  must be an airplane", {
-		unit.getType().shouldBeEqualTo "airplane"
+	and
+	
+	when "the speed is set to 140", {
+		unit.setProperty("speed", 140)
+	}
+	
+	when "the speed is changed to 160", {
+		unit.setProperty("speed", 160)
+	}
+	
+	then "the speed must be 160", {
+		unit.getProperty("speed").shouldBeEqualTo 160
+	}
+}
+
+scenario "query for a property when none are set", {
+	it_behaves_as "shared behaviors"
+	
+	then "asking for a property must throw an exception", {
+		ensureThrows(RuntimeException) {
+			unit.getProperty("speed")
+		}
+	}
+}
+
+scenario "query for a property when specific property is not set", {
+	it_behaves_as "shared behaviors"
+	
+	when "the speed is set to 160", {
+		unit.setProperty("speed", 160)
+	}
+	
+	then "asking for altitude property must throw an exception", {
+		ensureThrows(RuntimeException) {
+			unit.getProperty("altitude")
+		}
+	}
+}
+
+scenario "adding a weapon to a unit", {
+	it_behaves_as "shared behaviors"
+	
+	when "a weapon is added to the unit", {
+		weapon = new Weapon()
+		unit.addWeapon(weapon)
+	}
+	
+	then "asking for altitude property must throw an exception", {
+		ensure(unit.weapons) {
+			contains(weapon)
+		}
 	}
 }
